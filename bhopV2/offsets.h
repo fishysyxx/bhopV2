7b7e68e23030e1433cd7c558aea48e962c915cfc
fishysyxx <fishysyxx@gmail.com>
2025-03-27 20:06:23 -0400

    Add project files.

---
+++ bhopV2/offsets.h
@@ -0,0 +1,15 @@
+#ifndef OFFSETS_H
+#define OFFSETS_H
+
+#include "include.h"
+
+inline Wmemory cs2(L"cs2.exe");
+//update from a2x dumper
+namespace Offsets
+{
+	adr64 client = cs2.GetModuleBaseAddress(L"client.dll");
+
+	const uintptr_t dwForceJump = 0x1885C80;//bhop
+}
+
+#endif
